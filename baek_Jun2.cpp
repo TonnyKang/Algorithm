625635ff8a0e7b5023b6094989e5004fb9678b32
Dong Gwon Kang <122734760+TonnyKang@users.noreply.github.com>
2023-02-28 16:54:43 +0900

    finding death numbers from the smallest
    using % and / to see if 666 is in the string(actually an int)

---
+++ baek_Jun2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int n;
+
+int main() {
+	cin >> n;
+	int cnt = 0;
+	int i = 1;
+	while (n != cnt) {
+		int temp = i;
+		while (temp) {
+			if (temp % 1000 == 666) {
+				cnt++;
+				break;
+			}
+			else {
+				temp = temp / 10;
+			}
+		}
+		i++;
+	}
+	cout << i-1;
+}
